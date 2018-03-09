5205efd31c8c54badfe88e15055859ee3476f0a1
Szabó Olivér <haverkulum@gmail.com>
2018-03-09 13:59:41 +0100

    .

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -577,10 +577,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-// #define DEFAULT_ACCELERATION          950    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_ACCELERATION          350    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   950    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   350    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -590,8 +589,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
+#define DEFAULT_XJERK                  7.5
+#define DEFAULT_YJERK                  7.5
 #define DEFAULT_ZJERK                  0.3
 #define DEFAULT_EJERK                  5.0
 
